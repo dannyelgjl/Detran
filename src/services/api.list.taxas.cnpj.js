ccc0240bfc7d0d5ceac030dbc4c679bbb6ff8a6e
dannyelgjl <dannyelgjl@gmail.com>
2020-11-06 20:02:47 -0300

    criando api fake

---
+++ src/services/api.list.taxas.cnpj.js
@@ -0,0 +1,66 @@
+const taxas = [
+  {
+    codigo: 37,
+    descricao: "Credenciamento de clínica (médico / psicológico",
+    valorAtual: 391.83
+  },
+
+  {
+    codigo: 70,
+    descricao: "Credenciamento de Fabricantes de placas",
+    valorAtual: 211.80
+  },
+
+  {
+    codigo: 71,
+    descricao: "Renovação de Fabricante de placas",
+    valorAtual: 211.80
+  },
+
+  {
+    codigo: 72,
+    descricao: "Credenciamento de Oficinas / Desmanches",
+    valorAtual: 211.80
+  },
+
+  {
+    codigo: 73,
+    descricao: "Renovação Anual de Oficinas / Desmanches",
+    valorAtual: 211.80
+  },
+
+  {
+    codigo: 74,
+    descricao: "Renovação Anual de escritório despachante",
+    valorAtual: 211.80
+  },
+  {
+    codigo: 75,
+    descricao: "Credenciamento de escritório de despachantes",
+    valorAtual: 423.60
+  },
+  {
+    codigo: 66,
+    descricao: "Credenciamento de agente financeiro",
+    valorAtual: 875.44
+  },
+
+  {
+    codigo: 67,
+    descricao: "Renovação Anual de agente financeiro",
+    valorAtual: 437.72
+  },
+  
+  {
+    codigo: 16,
+    descricao: "Credenciamento de CFC",
+    valorAtual: 391.83
+  },
+
+  {
+    codigo: 17,
+    descricao: "Renovação Anual de CFC",
+    valorAtual: 391.83
+  }
+
+];
