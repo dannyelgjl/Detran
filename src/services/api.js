976fcd0426114ef322f5474caaada18086cb9f1e
devlyncas <devlyncas@salutho.com>
2021-01-05 13:14:54 -0300

    API adjustments and application tests

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://api.detranpi.brasiltecnologia.net.br/api/detran/v1/",
+  baseURL: "http://api.detranpi.brasiltecnologia.net.br/api/detran/totem/v1/",
 });
 
 export default api;
